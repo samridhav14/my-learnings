0e898bb71685e7beeb6d14ae43c2a9396d6ef3d7
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2022-12-31 14:29:16 +0530

    removed notes

--- html my learning/notes.cpp
+++
@@ -1,5 +0,0 @@
-//html is hypertext markup language it is used to structure and describe the content of web page
-// it is used to describe something that is ehy it is markup language
-// it contains diff type of element that consist of para links headings images vedio etc
-// web browsers understand html and render html code as webpages 
-//
